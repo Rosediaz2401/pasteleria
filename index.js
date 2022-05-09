d6220c187579b7419c6703839e273a5ab5157683
rosediaz <rosa.diaz.aguilar@gmail.com>
2022-05-09 12:59:45 -0500

    pagina angelisa

---
+++ index.js
@@ -0,0 +1,8 @@
+import hamburgerMenu from "./menu_hamburguesa.js";
+// import map from "./map.js";
+
+const d=document;
+d.addEventListener("DOMContentLoaded",e=>{
+    hamburgerMenu(".panel-btn",".panel", ".menu a");
+    // map(".mapa")
+});
